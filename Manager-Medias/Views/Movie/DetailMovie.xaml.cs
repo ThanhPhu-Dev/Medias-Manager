1174a11253fade6846780c8698482ccee0b2eb42
ThanhPhu-Dev <tphu2000@gmail.com>
2021-05-20 11:06:09 +0700

    tạo class

---
+++ Manager-Medias/Views/Movie/DetailMovie.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Manager_Medias.Views.Movie
+{
+    /// <summary>
+    /// Interaction logic for DetailMovie.xaml
+    /// </summary>
+    public partial class DetailMovie : Window
+    {
+        public DetailMovie()
+        {
+            InitializeComponent();
+        }
+    }
+}

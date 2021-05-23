ec82ef8357b61bae4cd0ca36ad08a2b16e92f166
dev <ndkhoa171@gmail.com>
2021-05-23 19:35:08 +0700

    trang chủ

---
+++ Manager-Medias/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Manager_Medias.Views.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Manager_Medias
+{
+    public partial class MainWindow : Window
+    {      
+        //KHOA: hiển thị user control tương ứng từ control grid 
+        internal void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+            if (screen != null)
+            {
+                grid.Children.Clear();
+                grid.Children.Add(screen);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            grid.Children.Add(new Home(this)); //KHOA: truyền this để gọi hàm SwitchScreen từ các user control
+        }
+    }
+}
---
+++ Manager-Medias/Views/Audio/DetailAudioView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Manager_Medias.Views.Audio
+{
+    /// <summary>
+    /// Interaction logic for DetailAudioView.xaml
+    /// </summary>
+    public partial class DetailAudioView : UserControl
+    {
+        public DetailAudioView()
+        {
+            InitializeComponent();
+            List<test> items = new List<test>();
+            items.Add(new test() { stt = "1", name = "Sắp 30", time = "4:05" });
+            items.Add(new test() { stt = "2", name = "Giờ này mà sao còn", time = "4:05" });
+            items.Add(new test() { stt = "3", name = "Trốn tìm", time = "4:05" });
+            items.Add(new test() { stt = "1", name = "Sắp 30", time = "4:05" });
+            items.Add(new test() { stt = "2", name = "Giờ này mà sao còn", time = "4:05" });
+            items.Add(new test() { stt = "3", name = "Trốn tìm", time = "4:05" });
+            items.Add(new test() { stt = "1", name = "Sắp 30", time = "4:05" });
+            items.Add(new test() { stt = "2", name = "Giờ này mà sao còn", time = "4:05" });
+            items.Add(new test() { stt = "3", name = "Trốn tìm", time = "4:05" });
+            items.Add(new test() { stt = "1", name = "Sắp 30", time = "4:05" });
+            items.Add(new test() { stt = "2", name = "Giờ này mà sao còn", time = "4:05" });
+            items.Add(new test() { stt = "3", name = "Trốn tìm", time = "4:05" });
+            DataContext = items;
+        }
+    }
+    public class test
+    {
+        public string stt { get; set; }
+        public string name { get; set; }
+        public string time { get; set; }
+    }
+}

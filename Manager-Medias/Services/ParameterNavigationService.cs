667b4f8b8d611f96c1a3228999a0a39ea03a4289
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-24 20:41:05 +0700

    navigate home customer

---
+++ Manager-Medias/Services/ParameterNavigationService.cs
@@ -0,0 +1,27 @@
+﻿using Manager_Medias.Stores;
+using Manager_Medias.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_Medias.Services
+{
+    public class ParameterNavigationService<TParameter, TViewModel> where TViewModel : BaseViewModel
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TParameter, TViewModel> _createViewModel;
+
+        public ParameterNavigationService(NavigationStore navigationStore, Func<TParameter, TViewModel> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public void Navigate(TParameter parameter)
+        {
+            _navigationStore.ContentViewModel = _createViewModel(parameter);
+        }
+    }
+}

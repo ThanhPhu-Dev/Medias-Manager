23ae9c5ea7d505d5fd1b63ef7c50ae3bc277ca29
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-25 21:46:57 +0700

    account navigate

---
+++ Manager-Medias/Models/MovieCarouselModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_Medias.Models
+{
+    public class MovieCarouselModel
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Manager-Medias/ViewModels/Customer/AccountManagerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Manager_Medias.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_Medias.ViewModels.Customer
+{
+    public class AccountManagerViewModel : BaseViewModel
+    {
+        public AccountManagerViewModel()
+        {
+        }
+
+        public AccountManagerViewModel(UserStore userStore)
+        {
+        }
+    }
+}

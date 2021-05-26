c2d104ed4e0e3c866be93929d3cb28a350fe0586
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-26 16:24:14 +0700

    name validation

--- Manager-Medias/App.xaml.cs
+++ Manager-Medias/App.xaml.cs
@@ -36,10 +36,10 @@ namespace Manager_Medias
 
             //UserStore userStore = new UserStore(currentUser);
 
-            navigationStore.ContentViewModel = new DetailMovieViewModel(userStore);
+            //navigationStore.ContentViewModel = new DetailMovieViewModel(userStore);
             //navigationStore.ContentViewModel = new DetailAudioViewModel();
             //navigationStore.ContentViewModel = new SignIn();
-            navigationStore.CurrentViewModel = new MainLayoutViewModel(userStore, navigationStore);
+            //navigationStore.CurrentViewModel = new MainLayoutViewModel(userStore, navigationStore);
 
             MainWindow = new MainWindow()
             {

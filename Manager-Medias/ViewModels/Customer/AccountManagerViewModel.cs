86106cee25dde575d5f178b72ad3df705cddc8ec
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-26 14:05:09 +0700

    Navigate back command

--- Manager-Medias/ViewModels/Customer/AccountManagerViewModel.cs
+++ Manager-Medias/ViewModels/Customer/AccountManagerViewModel.cs
@@ -1,19 +1,29 @@
-﻿using Manager_Medias.Stores;
+﻿using Manager_Medias.Commands;
+using Manager_Medias.Stores;
+using Manager_Medias.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Manager_Medias.ViewModels.Customer
 {
     public class AccountManagerViewModel : BaseViewModel
     {
-        public AccountManagerViewModel()
+        public ICommand SaveCmd { get; }
+        public ICommand NavigateHomeCmd { get; }
+
+        public AccountManagerViewModel(UserStore userStore, NavigationStore navigationStore)
         {
+            _navigationStore = navigationStore;
+            SaveCmd = new RelayCommand<Object[]>(ActionSave);
+
+            NavigateHomeCmd = new NavigateCommand<HomeViewModel>(new NavigationService<HomeViewModel>(_navigationStore, () => new HomeViewModel()));
         }
 
-        public AccountManagerViewModel(UserStore userStore)
+        public void ActionSave(Object[] values)
         {
         }
     }

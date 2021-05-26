88a4aa484385d6edfa2747ff9eaae1526e8bc1fa
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-05-26 15:06:41 +0700

    play pause video xong giao diện chưa sửa

--- Manager-Medias/DB/query.sql
+++ Manager-Medias/DB/query.sql
@@ -224,5 +224,5 @@ update Medias set Lvl = 1 where Id = 7
 select * from medias as m join Levels l on l.Id = m.Lvl
 insert into Levels(Name,Price) values('Vàng', 200000)
 insert into Movies (Id, IdCategory,IMDB,Likes,Name,NumberOfViews,Poster,Age,Description,Season,Time,Video)
-values (7,1,6.5,4,N'Super Hero', 23, 'postermovieHuter.jpg', 18,N'Super hero hân hạnh tài trợ', 'kn92','12:30','video.mp4')
+values (7,1,6.5,4,N'Super Hero', 23, 'postermovieHuter.jpg', 18,N'Super hero hân hạnh tài trợ', 'kn92','00:12:30','video.mp4')
 

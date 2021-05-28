902c486ed515913cf8e43f09d37bd2092c98e551
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-28 20:55:43 +0700

    Cũng chẳng nhớ đã làm gì

---
+++ Manager-Medias/ViewModels/Customer/PaymentHistoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Manager_Medias.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_Medias.ViewModels.Customer
+{
+    public class PaymentHistoryViewModel : BaseViewModel
+    {
+        public PaymentHistoryViewModel(UserStore userStore)
+        {
+        }
+    }
+}

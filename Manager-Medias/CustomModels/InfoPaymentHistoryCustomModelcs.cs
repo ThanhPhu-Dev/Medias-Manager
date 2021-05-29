349ae92c71db59e4844a2383d80a65d538a7c9f7
ThanhPhu-Dev <tphu2000@gmail.com>
2021-05-29 11:23:38 +0700

    xong payment

---
+++ Manager-Medias/CustomModels/InfoPaymentHistoryCustomModelcs.cs
@@ -0,0 +1,28 @@
+﻿using Manager_Medias.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_Medias.CustomModels
+{
+    public class InfoPaymentHistoryCustomModelcs
+    {
+        public int STT { get; set; }
+        public string DateOfPayment { get; set; }
+        public string Note { get; set; }
+        public long Price { get; set; }
+
+        public InfoPaymentHistoryCustomModelcs()
+        {
+
+        }
+        public InfoPaymentHistoryCustomModelcs(Payment_History pm)
+        {
+            DateOfPayment = pm.DateOfPayment.Value.ToString("d");
+            Note = pm.Note;
+            Price = long.Parse(pm.Price.Value.ToString());
+        }
+    }
+}
--- Manager-Medias/ViewModels/Customer/PaymentHistoryViewModel.cs
+++ Manager-Medias/ViewModels/Customer/PaymentHistoryViewModel.cs
@@ -3,6 +3,7 @@ using Manager_Medias.Models;
 using Manager_Medias.Stores;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -51,8 +52,8 @@ namespace Manager_Medias.ViewModels.Customer
             }
         }
 
-        private List<InfoDetailPaymentCustomModels> _infoPM;
-        public List<InfoDetailPaymentCustomModels> InfoPM
+        private ObservableCollection<InfoPaymentHistoryCustomModelcs> _infoPM;
+        public ObservableCollection<InfoPaymentHistoryCustomModelcs> InfoPM
         {
             get => _infoPM;
             set
@@ -64,14 +65,20 @@ namespace Manager_Medias.ViewModels.Customer
 
         public void loaded()
         {
-            Email = usercurrent.Email;
+            _email = usercurrent.Email;
             _name = usercurrent.CurrentProfile.Name;
             _numberCard = usercurrent.CurrentUser.NumberCard;
+            InfoPM = new ObservableCollection<InfoPaymentHistoryCustomModelcs>();
             using (var db = new MediasManangementEntities())
             {
-                var lst = db.Payment_History.Where(p => p.Email == usercurrent.Email).ToList();
-                InfoDetailPaymentCustomModels ele = new InfoDetailPaymentCustomModels();
-                
+                var lst = db.Payment_History.Where(p => p.Email == usercurrent.Email).OrderByDescending(s => s.DateOfPayment).ToList();
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    InfoPaymentHistoryCustomModelcs info = new InfoPaymentHistoryCustomModelcs(lst[i] as Payment_History);
+                    info.STT = i + 1;
+                    InfoPM.Add(info);
+                }
+
             }
         }
     }

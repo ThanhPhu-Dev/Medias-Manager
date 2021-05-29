4fa82f063ee6292aaa17c656345d19a82958f9a5
ThanhPhu-Dev <tphu2000@gmail.com>
2021-05-29 10:06:10 +0700

    payment

--- Manager-Medias/ViewModels/Customer/PaymentHistoryViewModel.cs
+++ Manager-Medias/ViewModels/Customer/PaymentHistoryViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Manager_Medias.Stores;
+﻿using Manager_Medias.CustomModels;
+using Manager_Medias.Models;
+using Manager_Medias.Stores;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +11,68 @@ namespace Manager_Medias.ViewModels.Customer
 {
     public class PaymentHistoryViewModel : BaseViewModel
     {
+        UserStore usercurrent;
         public PaymentHistoryViewModel(UserStore userStore)
         {
+            this.usercurrent = userStore;
+            loaded();
+
+        }
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _numberCard;
+        public string NumberCard
+        {
+            get => _numberCard;
+            set
+            {
+                _numberCard = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<InfoDetailPaymentCustomModels> _infoPM;
+        public List<InfoDetailPaymentCustomModels> InfoPM
+        {
+            get => _infoPM;
+            set
+            {
+                _infoPM = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void loaded()
+        {
+            Email = usercurrent.Email;
+            _name = usercurrent.CurrentProfile.Name;
+            _numberCard = usercurrent.CurrentUser.NumberCard;
+            using (var db = new MediasManangementEntities())
+            {
+                var lst = db.Payment_History.Where(p => p.Email == usercurrent.Email).ToList();
+                InfoDetailPaymentCustomModels ele = new InfoDetailPaymentCustomModels();
+                
+            }
         }
     }
 }
--- Manager-Medias/Views/Account/PaymentHistoryView.xaml.cs
+++ Manager-Medias/Views/Account/PaymentHistoryView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Manager_Medias.ViewModels.Customer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

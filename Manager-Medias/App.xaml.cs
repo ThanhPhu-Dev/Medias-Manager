c567b9f56da7074b6a9b6b24096d7ffc35a61596
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-30 15:37:51 +0700

    fix layout

--- Manager-Medias/App.xaml.cs
+++ Manager-Medias/App.xaml.cs
@@ -23,8 +23,8 @@ namespace Manager_Medias
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDUwNjgwQDMxMzkyZTMxMmUzMEFCRUZSUzU4RGNhTGo0eHkvMW9YYVJkMUd5UXhxbTdaNTdtSFFJSS84dlU9");
             // Init location
             NavigationStore navigationStore = new NavigationStore();
-            navigationStore.ContentViewModel = new HomeViewModel();
-            navigationStore.CurrentViewModel = new HomeViewModel();
+            navigationStore.ContentViewModel = new GuestHomeViewModel();
+            navigationStore.CurrentViewModel = new GuestMainViewModel(navigationStore);
             //User currentUser = new User
             //{
             //    Code = "usercode",

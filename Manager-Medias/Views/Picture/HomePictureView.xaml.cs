9f3d238a42a06c8e975ae2ef08c3cd0f226ce1e0
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-04 08:39:02 +0700

    tao file làm homePictureView

---
+++ Manager-Medias/Views/Picture/HomePictureView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Manager_Medias.Views.Picture
+{
+    /// <summary>
+    /// Interaction logic for HomePicture.xaml
+    /// </summary>
+    public partial class HomePicture : UserControl
+    {
+        public HomePicture()
+        {
+            InitializeComponent();
+        }
+        public static childItem FindVisualChild<childItem>(DependencyObject obj)
+                where childItem : DependencyObject
+        {
+            // Iterate through all immediate children
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+
+            return null;
+        }
+        private void ItemsControl_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is ItemsControl && !e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                eventArg.Source = sender;
+                var parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
+        }
+    }
+}
